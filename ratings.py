58fab89912ba236c16b985148a3ff737b1b8517f
Samantha Wette <samantha.wette@gmail.com>
2022-04-04 15:23:00 -0700

    Prints ratings alphabetically

--- ratings.py
+++ ratings.py
@@ -1,4 +1,21 @@
 """Restaurant rating lister."""
 
+def read_ratings(file_name):
+    content = open(file_name)
+    restaurant_information = {}
+    ratings = []
+    for line in content:
+        line = line.rstrip()
+        ratings.append(line.split(":"))
+    content.close()      
 
-# put your code here
+    for rating in ratings:
+        name = rating[0]
+        score = rating[1]
+        restaurant_information[name] = score
+    
+    for name, score in sorted(restaurant_information.items()):
+        print(f"{name} is rated at {score}.")
+
+
+read_ratings('scores.txt')
