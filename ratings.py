dea4f9afec1960a1a804b4ba9232567806274b13
Samantha Wette <samantha.wette@gmail.com>
2022-04-04 15:51:00 -0700

    utilizes dictionary without list

--- ratings.py
+++ ratings.py
@@ -3,19 +3,32 @@
 def read_ratings(file_name):
     content = open(file_name)
     restaurant_information = {}
-    ratings = []
+    # ratings = []
     for line in content:
         line = line.rstrip()
-        ratings.append(line.split(":"))
+        line = line.split(":")
+        # print(f"=================={line}")        
+        # print(restaurant_information)
+        # ratings.append(line.split(":"))
+        restaurant_information[line[0]] = int(line[1]) 
+
+           
     content.close()      
 
-    for rating in ratings:
-        name = rating[0]
-        score = rating[1]
-        restaurant_information[name] = score
+    added_restaurant = input('What restaurant would you like to rate? ')
+    added_rating = input('How would you rate that restaurant from 1 to 5? ')
+    restaurant_information[added_restaurant] = int(added_rating)
+
+    #ratings.append([added_restaurant, added_rating])
+
+    # for rating in ratings:
+    #     name = rating[0]
+    #     score = rating[1]
+    #     restaurant_information[name] = score
     
     for name, score in sorted(restaurant_information.items()):
         print(f"{name} is rated at {score}.")
 
+    
 
 read_ratings('scores.txt')
